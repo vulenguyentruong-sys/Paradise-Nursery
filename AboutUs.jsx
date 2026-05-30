17a974a71affc3af80538b05629ba1e4d4125812
vulenguyentruong-sys <vulenguyentruong@gmail.com>
2026-05-30 18:14:55 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+      <p>At Paradise Nursery, we are passionate about bringing nature into your home. We offer a wide variety of houseplants, from air-purifying wonders to low-maintenance succulents. Our mission is to help you create your own indoor paradise, one plant at a time.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
